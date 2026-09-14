305edaa5e8282fead639965664b507ed3873c3c5
Felipe Azevedo <felipeasantos10@hotmail.com>
2026-09-14 16:10:10 -0300

    feat: app routes

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,13 +1,7 @@
-import { Text } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { Redirect } from "expo-router";
 
+const Home = () => {
+  return <Redirect href="/(auth)/welcome" />;
+};
 
-export default function HomeScreen() {
-  return (
-    <SafeAreaView className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">
-        Welcome to Nativewind!
-      </Text>
-    </SafeAreaView>
-  );
-}
+export default Home;
